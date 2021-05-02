ed5a3a0ba3574c686997ddfc540265244817596e
jessegj <jen18055@byui.edu>
2021-05-01 21:53:42 -0600

    contact-list

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from '../contact.model';
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+
+  contact: Contact[] = [
+    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', './images/LarsPedersen.JPG', 'NULL')
+  ];
 
   constructor() { }
 
