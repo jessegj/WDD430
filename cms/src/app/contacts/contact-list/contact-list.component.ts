0dbe72891be46304e6d04e3caca92dc00edfdc7c
jessegj <jen18055@byui.edu>
2021-05-05 18:13:27 -0600

    contact list showing

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -10,8 +10,8 @@ import { Contact } from '../contact.model';
 export class ContactListComponent implements OnInit {
 
   contact: Contact[] = [
-    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', './images/LarsPedersen.JPG', 'NULL'),
-    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', './images/AndersPedersen.JPG', 'NULL')
+    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../images/LarsPedersen.JPG', 'NULL'),
+    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../images/AndersPedersen.JPG', 'NULL')
   ];
 
   constructor() { }
