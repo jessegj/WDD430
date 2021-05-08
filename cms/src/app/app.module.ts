2c0be6f2eba4a7289533a2ca96698398299a7681
jessegj <jen18055@byui.edu>
2021-05-08 17:16:01 -0600

    small changes in ngFor names

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { HeaderComponent } from './header.component';
-import { ContactItemComponent } from './contact-item/contact-item.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
 
 @NgModule({
   declarations: [
