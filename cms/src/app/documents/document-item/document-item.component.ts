c0aae5d1b14c5eaa1293a4b1b82c2d388df0296d
jessegj <jen18055@byui.edu>
2021-05-18 18:43:56 -0600

    finished wk4

--- cms/src/app/documents/document-item/document-item.component.ts
+++ cms/src/app/documents/document-item/document-item.component.ts
@@ -8,7 +8,7 @@ import { DocumentModel } from '../document.model';
 })
 export class DocumentItemComponent implements OnInit {
 
-  @Input() document: DocumentModel | undefined;
+  @Input() document: DocumentModel;
 
   constructor() { }
 
