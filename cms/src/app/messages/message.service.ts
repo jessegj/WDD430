8b25cd2248c0c9afb61da60eb66aa1d7689c1bf0
jessegj <jen18055@byui.edu>
2021-06-18 21:35:44 -0600

    Message connected to the database

--- cms/src/app/messages/message.service.ts
+++ cms/src/app/messages/message.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,29 @@ export class MessageService {
 
   messages: Message[] = [];
   messageChangedEvent = new EventEmitter<Message[]>();
+  maxMessageId: any;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.messages = MOCKMESSAGES;
+
+    this.http
+      .get<Message[]>(
+        'https://jessegj-cmsproject-default-rtdb.firebaseio.com/messages.json'
+      )
+        //success method
+
+      .subscribe(
+        (messages: Message[]) => {
+          this.messages = messages;
+          this.maxMessageId = this.getMaxId();
+          messages.sort((a, b) => (a.id < b.id ? 1 : 0));
+          this.messageChangedEvent.next(this.messages.slice());
+        },
+        //error method
+        (error: any) => {
+          console.log(error);
+        }
+      );
    }
    getMessages(): Message[] {
     return this.messages;
@@ -27,6 +48,29 @@ export class MessageService {
   }
   addMessage(message: Message) {
     this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
+    //this.messageChangedEvent.emit(this.messages.slice());
+    this.storeMessages();
+  }
+  getMaxId() {
+    let maxId = 0;
+
+    for (let message of this.messages) {
+      let currentId = parseInt(message.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+  }
+  storeMessages() {
+    const messages = JSON.stringify(this.messages.slice());
+    const headers = new HttpHeaders();
+    headers.set('Content-Type', 'application/json');
+    this.http
+      .put<Message[]>(
+        'https://jessegj-cmsproject-default-rtdb.firebaseio.com/messages.json',
+        this.messages)
+      .subscribe(() => {
+        this.messageChangedEvent.next(this.messages.slice());
+      });
   }
 }
