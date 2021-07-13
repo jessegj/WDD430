376fdf9d07650bde79c6f051de735f897831c1cd
jessegj <jen18055@byui.edu>
2021-07-12 22:38:25 -0600

    Start of HTML

--- lego-project/src/app/app.module.ts
+++ lego-project/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InventoryComponent,
+    ProjectPageComponent,
   ],
   imports: [
     BrowserModule
