202c1f711b12b016510455ef205c0706480afc5f
jessegj <jen18055@byui.edu>
2021-07-15 23:12:47 -0600

    Beginning CRUD in inventory

--- lego-project/src/app/inventory/inventory.service.ts
+++ lego-project/src/app/inventory/inventory.service.ts
@@ -10,6 +10,21 @@ export class InventoryService {
   inventories: Inventory[] = [];
 
   constructor() { }
+
+  getPieces(): Inventory[] {
+    return this.inventories;
+  }
+
+  getPiece(id: string): Inventory | null {
+    for (let inventory of this.inventories) {
+      if (inventory.id == id) {
+        return inventory;
+      }
+    }
+    return null;
+  }
+
+
 }
 
 
